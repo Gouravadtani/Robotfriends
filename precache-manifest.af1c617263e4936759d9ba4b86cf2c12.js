9b987da0a4d4080eb9310fcff8135be4c6b70c49
Gourav Adtani <65698138+Gouravadtani@users.noreply.github.com>
2020-07-25 09:32:20 +0530

    i have added all files

---
+++ precache-manifest.af1c617263e4936759d9ba4b86cf2c12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1b76d8c230cd1ed94727de890c6e1e",
+    "url": "/Robotfriends/index.html"
+  },
+  {
+    "revision": "4a4f454c2857d9596850",
+    "url": "/Robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d12a8786a411b9550536",
+    "url": "/Robotfriends/static/css/main.8148c6d4.chunk.css"
+  },
+  {
+    "revision": "4a4f454c2857d9596850",
+    "url": "/Robotfriends/static/js/2.fbcc8165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robotfriends/static/js/2.fbcc8165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12a8786a411b9550536",
+    "url": "/Robotfriends/static/js/main.6de4c614.chunk.js"
+  },
+  {
+    "revision": "a8542bcc3885f9648168",
+    "url": "/Robotfriends/static/js/runtime-main.9690cc26.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Grahan',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@lissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementie Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.Oconner@kozy.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsy Dietrich',
+    username: 'Kamren',
+    email: 'Chelsy@kamren.biz'
+  },
+  {
+    id: 6,
+    name: 'Leena Adtani',
+    username: 'Leens',
+    email: 'Leena@adtani.biz'
+  },
+  {
+    id: 7,
+    name: 'Jitesh Adtani',
+    username: 'Jeetu',
+    email: 'jeetu@adtani.biz'
+  },
+  {
+    id: 8,
+    name: 'Jiya Adtani',
+    username: 'Jiya',
+    email: 'Jiya@adtani.biz'
+  },
+  {
+    id: 9,
+    name: 'Ishant Adtani',
+    username: 'Leens',
+    email: 'Ishant@adtani.biz'
+  },
+  {
+    id: 10,
+    name: 'Manish Adtani',
+    username: 'Mana',
+    email: 'Manish@adtani.biz'
+  },
+]
